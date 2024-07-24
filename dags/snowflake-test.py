ae641a1a6c9b429e8db301b84cfe342ff59ad79c
Schmidt, Friedrich <Friedrich.Schmidt@europapark.de>
2024-07-24 14:50:39 +0200

    added new snowflake dag

--- dags/snowflake-test.py
+++ dags/snowflake-test.py
@@ -1,79 +1,225 @@
-"""
-## Astronaut ETL example DAG
-
-This DAG queries the list of astronauts currently in space from the 
-Open Notify API and prints each astronaut's name and flying craft.
-
-There are two tasks, one to get the data from the API and save the results,
-and another to print the results. Both tasks are written in Python using
-Airflow's TaskFlow API, which allows you to easily turn Python functions into
-Airflow tasks, and automatically infer dependencies and pass data.
-
-The second task uses dynamic task mapping to create a copy of the task for
-each Astronaut in the list retrieved from the API. This list will change
-depending on how many Astronauts are in space, and the DAG will adjust 
-accordingly each time it runs.
-
-For more explanation and getting started instructions, see our Write your 
-first DAG tutorial: https://docs.astronomer.io/learn/get-started-with-airflow
-
-![Picture of the ISS](https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2010/02/space_station_over_earth/10293696-3-eng-GB/Space_Station_over_Earth_card_full.jpg)
-"""
-
-from airflow import Dataset
-from airflow.decorators import dag, task
-from pendulum import datetime
-import requests
-
-#Define the basic parameters of the DAG, like schedule and start_date
-@dag(
-    start_date=datetime(2024, 1, 1),
-    schedule="@daily",
-    catchup=False,
-    doc_md=__doc__,
-    default_args={"owner": "Astro", "retries": 3},
-    tags=["example"],
+from airflow import DAG
+from airflow.models.baseoperator import chain
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.common.sql.operators.sql import (
+    SQLColumnCheckOperator,
+    SQLTableCheckOperator,
 )
-def example_astronauts():
-    #Define tasks
-    @task(
-        #Define a dataset outlet for the task. This can be used to schedule downstream DAGs when this task has run.
-        outlets=[Dataset("current_astronauts")]
-    )  # Define that this task updates the `current_astronauts` Dataset
-    def get_astronauts(**context) -> list[dict]:
-        """
-        This task uses the requests library to retrieve a list of Astronauts 
-        currently in space. The results are pushed to XCom with a specific key
-        so they can be used in a downstream pipeline. The task returns a list
-        of Astronauts to be used in the next task.
-        """
-        r = requests.get("http://api.open-notify.org/astros.json")
-        number_of_people_in_space = r.json()["number"]
-        list_of_people_in_space = r.json()["people"]
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from pendulum import datetime
+from airflow.utils.task_group import TaskGroup
+import include.sql.tutorial_sql_statements as sql_stmts
 
-        context["ti"].xcom_push(
-            key="number_of_people_in_space", value=number_of_people_in_space
-        )
-        return list_of_people_in_space
+SNOWFLAKE_FORESTFIRE_TABLE = "forestfires"
+SNOWFLAKE_COST_TABLE = "costs"
+SNOWFLAKE_FORESTFIRE_COST_TABLE = "forestfire_costs"
 
-    @task
-    def print_astronaut_craft(greeting: str, person_in_space: dict) -> None:
-        """
-        This task creates a print statement with the name of an 
-        Astronaut in space and the craft they are flying on from 
-        the API request results of the previous task, along with a 
-        greeting which is hard-coded in this example.
-        """
-        craft = person_in_space["craft"]
-        name = person_in_space["name"]
+SNOWFLAKE_CONN_ID = "snowflake_default"
 
-        print(f"{name} is currently in space flying on the {craft}! {greeting}")
+ROW_COUNT_CHECK = "COUNT(*) = 9"
 
-    #Use dynamic task mapping to run the print_astronaut_craft task for each 
-    #Astronaut in space
-    print_astronaut_craft.partial(greeting="Hello! :)").expand(
-        person_in_space=get_astronauts() #Define dependencies using TaskFlow API syntax
+with DAG(
+    "complex_snowflake_example",
+    description="""
+        Example DAG showcasing loading, transforming, 
+        and data quality checking with multiple datasets in Snowflake.
+    """,
+    doc_md=__doc__,
+    start_date=datetime(2022, 12, 1),
+    schedule=None,
+    # defining the directory where SQL templates are stored
+    template_searchpath="/usr/local/airflow/include/sql/",
+    catchup=False,
+) as dag:
+    """
+    #### Snowflake table creation
+    Create the tables to store sample data.
+    """
+    create_forestfire_table = SnowflakeOperator(
+        task_id="create_forestfire_table",
+        sql=sql_stmts.create_forestfire_table,
+        params={"table_name": SNOWFLAKE_FORESTFIRE_TABLE},
     )
 
-#Instantiate the DAG
-example_astronauts()
+    create_cost_table = SnowflakeOperator(
+        task_id="create_cost_table",
+        sql=sql_stmts.create_cost_table,
+        params={"table_name": SNOWFLAKE_COST_TABLE},
+    )
+
+    create_forestfire_cost_table = SnowflakeOperator(
+        task_id="create_forestfire_cost_table",
+        sql=sql_stmts.create_forestfire_cost_table,
+        params={"table_name": SNOWFLAKE_FORESTFIRE_COST_TABLE},
+    )
+
+    """
+    #### Insert data
+    Insert data into the Snowflake tables using existing SQL queries
+    stored in the include/sql/snowflake_examples/ directory.
+    """
+    load_forestfire_data = SnowflakeOperator(
+        task_id="load_forestfire_data",
+        sql=sql_stmts.load_forestfire_data,
+        params={"table_name": SNOWFLAKE_FORESTFIRE_TABLE},
+    )
+
+    load_cost_data = SnowflakeOperator(
+        task_id="load_cost_data",
+        sql=sql_stmts.load_cost_data,
+        params={"table_name": SNOWFLAKE_COST_TABLE},
+    )
+
+    load_forestfire_cost_data = SnowflakeOperator(
+        task_id="load_forestfire_cost_data",
+        sql=sql_stmts.load_forestfire_cost_data,
+        params={"table_name": SNOWFLAKE_FORESTFIRE_COST_TABLE},
+    )
+
+    """
+    #### Transform
+    Transform the forestfire_costs table to perform
+    sample logic.
+    """
+    transform_forestfire_cost_table = SnowflakeOperator(
+        task_id="transform_forestfire_cost_table",
+        sql=sql_stmts.transform_forestfire_cost_table,
+        params={"table_name": SNOWFLAKE_FORESTFIRE_COST_TABLE},
+    )
+
+    """
+    #### Quality checks
+    Perform data quality checks on the various tables.
+    """
+    with TaskGroup(
+        group_id="quality_check_group_forestfire",
+        default_args={
+            "conn_id": SNOWFLAKE_CONN_ID,
+        },
+    ) as quality_check_group_forestfire:
+        """
+        #### Column-level data quality check
+        Run data quality checks on columns of the forestfire table
+        """
+        forestfire_column_checks = SQLColumnCheckOperator(
+            task_id="forestfire_column_checks",
+            table=SNOWFLAKE_FORESTFIRE_TABLE,
+            column_mapping={
+                "ID": {"null_check": {"equal_to": 0}},
+                "RH": {"max": {"leq_to": 100}},
+            },
+        )
+
+        """
+        #### Table-level data quality check
+        Run data quality checks on the forestfire table
+        """
+        forestfire_table_checks = SQLTableCheckOperator(
+            task_id="forestfire_table_checks",
+            table=SNOWFLAKE_FORESTFIRE_TABLE,
+            checks={"row_count_check": {"check_statement": ROW_COUNT_CHECK}},
+        )
+
+    with TaskGroup(
+        group_id="quality_check_group_cost",
+        default_args={
+            "conn_id": SNOWFLAKE_CONN_ID,
+        },
+    ) as quality_check_group_cost:
+        """
+        #### Column-level data quality check
+        Run data quality checks on columns of the forestfire table
+        """
+        cost_column_checks = SQLColumnCheckOperator(
+            task_id="cost_column_checks",
+            table=SNOWFLAKE_COST_TABLE,
+            column_mapping={
+                "ID": {"null_check": {"equal_to": 0}},
+                "LAND_DAMAGE_COST": {"min": {"geq_to": 0}},
+                "PROPERTY_DAMAGE_COST": {"min": {"geq_to": 0}},
+                "LOST_PROFITS_COST": {"min": {"geq_to": 0}},
+            },
+        )
+
+        """
+        #### Table-level data quality check
+        Run data quality checks on the forestfire table
+        """
+        cost_table_checks = SQLTableCheckOperator(
+            task_id="cost_table_checks",
+            table=SNOWFLAKE_COST_TABLE,
+            checks={"row_count_check": {"check_statement": ROW_COUNT_CHECK}},
+        )
+
+    with TaskGroup(
+        group_id="quality_check_group_forestfire_costs",
+        default_args={
+            "conn_id": SNOWFLAKE_CONN_ID,
+        },
+    ) as quality_check_group_forestfire_costs:
+        """
+        #### Column-level data quality check
+        Run data quality checks on columns of the forestfire table
+        """
+        forestfire_costs_column_checks = SQLColumnCheckOperator(
+            task_id="forestfire_costs_column_checks",
+            table=SNOWFLAKE_FORESTFIRE_COST_TABLE,
+            column_mapping={"AREA": {"min": {"geq_to": 0}}},
+        )
+
+        """
+        #### Table-level data quality check
+        Run data quality checks on the forestfire table
+        """
+        forestfire_costs_table_checks = SQLTableCheckOperator(
+            task_id="forestfire_costs_table_checks",
+            table=SNOWFLAKE_FORESTFIRE_COST_TABLE,
+            checks={
+                "row_count_check": {"check_statement": ROW_COUNT_CHECK},
+                "total_cost_check": {
+                    "check_statement": "land_damage_cost + \
+                    property_damage_cost + lost_profits_cost = total_cost"
+                },
+            },
+        )
+
+    """
+    #### Delete tables
+    Clean up the tables created for the example.
+    """
+    delete_forestfire_table = SnowflakeOperator(
+        task_id="delete_forestfire_table",
+        sql="delete_table.sql",
+        params={"table_name": SNOWFLAKE_FORESTFIRE_TABLE},
+    )
+
+    delete_cost_table = SnowflakeOperator(
+        task_id="delete_costs_table",
+        sql="delete_table.sql",
+        params={"table_name": SNOWFLAKE_COST_TABLE},
+    )
+
+    delete_forestfire_cost_table = SnowflakeOperator(
+        task_id="delete_forestfire_cost_table",
+        sql="delete_table.sql",
+        params={"table_name": SNOWFLAKE_FORESTFIRE_COST_TABLE},
+    )
+
+    begin = EmptyOperator(task_id="begin")
+    create_done = EmptyOperator(task_id="create_done")
+    load_done = EmptyOperator(task_id="load_done")
+    end = EmptyOperator(task_id="end")
+
+    chain(
+        begin,
+        [create_forestfire_table, create_cost_table, create_forestfire_cost_table],
+        create_done,
+        [load_forestfire_data, load_cost_data],
+        load_done,
+        [quality_check_group_forestfire, quality_check_group_cost],
+        load_forestfire_cost_data,
+        quality_check_group_forestfire_costs,
+        transform_forestfire_cost_table,
+        [delete_forestfire_table, delete_cost_table, delete_forestfire_cost_table],
+        end,
+    )
